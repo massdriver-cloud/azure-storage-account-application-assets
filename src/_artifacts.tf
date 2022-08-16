e3a0dcf5dd15a2b6e81120c9d33063951de9afc2
mclacore <michael@massdriver.cloud>
2022-08-16 11:26:14 -0600

    Artifact file and if/then conditionals

--- src/_artifacts.tf
+++ src/_artifacts.tf
@@ -1,28 +1,41 @@
-# locals {
-#   data_infrastructure = {
-#     ari = azurerm_storage_account.main.id
-#   }
-#   data_authentication = {
-#     endpoint          = azurerm_storage_account.main.primary_blob_endpoint
-#     connection_string = azurerm_storage_account.main.primary_connection_string
-#   }
-# }
+locals {
+  data_infrastructure = {
+    ari = azurerm_storage_account.main.id
+  }
+  data_authentication = {
+    endpoint          = azurerm_storage_account.main.primary_blob_endpoint
+    connection_string = azurerm_storage_account.main.primary_connection_string
+  }
+  data_security = {
+    iam = {
+      "blob-read" = {
+        role  = "Storage Blob Data Reader"
+        scope = azurerm_storage_account.main.id
+      },
+      "blob-read-write" = {
+        role  = "Storage Blob Data Contributor"
+        scope = azurerm_storage_account.main.id
+      }
+    }
+  }
+}
 
-# resource "massdriver_artifact" "authentication" {
-#   field                = ""
-#   provider_resource_id = azurerm_storage_account.main.id
-#   name                 = "Azure Storage Account ${var.md_metadata.name_prefix} (${azurerm_storage_account.main.id})"
-#   artifact = jsonencode(
-#     {
-#       data = {
-#         infrastructure = local.data_infrastructure
-#         authentication = local.data_authentication
-#       }
-#       specs = {
-#         azure = {
-#           region = azurerm_storage_account.main.location
-#         }
-#       }
-#     }
-#   )
-# }
+resource "massdriver_artifact" "azure_storage_account" {
+  field                = "azure_storage_account"
+  provider_resource_id = azurerm_storage_account.main.id
+  name                 = "Azure Storage Account ${var.md_metadata.name_prefix} (${azurerm_storage_account.main.id})"
+  artifact = jsonencode(
+    {
+      data = {
+        infrastructure = local.data_infrastructure
+        authentication = local.data_authentication
+        security       = local.data_security
+      }
+      specs = {
+        azure = {
+          region = azurerm_storage_account.main.location
+        }
+      }
+    }
+  )
+}
