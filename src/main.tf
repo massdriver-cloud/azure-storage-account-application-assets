92de93799647b6b64e176005aa88189081a71eed
Michael Lacore <michael@massdriver.cloud>
2022-09-14 16:24:46 -0600

    Fix for name length constraint issue (#9)
    * Fix for name length constraint issue
    
    * Adding replace regex
    
    * Small BridgeCrew compliance changes
    
    * Minor fix for UI

--- src/main.tf
+++ src/main.tf
@@ -1,21 +1,25 @@
 locals {
-  storage_account_name = join("", split("-", var.md_metadata.name_prefix))
+  max_length           = 24
+  storage_account_name = substr(replace(var.md_metadata.name_prefix, "/[^a-z0-9]/", ""), 0, local.max_length)
 }
 
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.storage.region
+  tags     = var.md_metadata.default_tags
 }
 
 resource "azurerm_storage_account" "main" {
-  name                = local.storage_account_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-  access_tier              = "Hot"
-  account_replication_type = var.redundancy.replication_type
+  name                              = local.storage_account_name
+  resource_group_name               = azurerm_resource_group.main.name
+  location                          = azurerm_resource_group.main.location
+  account_tier                      = "Standard"
+  account_kind                      = "StorageV2"
+  access_tier                       = "Hot"
+  account_replication_type          = var.redundancy.replication_type
+  public_network_access_enabled     = false
+  infrastructure_encryption_enabled = true
+  tags                              = var.md_metadata.default_tags
 
   identity {
     type = "SystemAssigned"
