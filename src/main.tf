9443eff01747b2cf39c5a79ed96b00f17a3a208f
mclacore <michael@massdriver.cloud>
2022-09-14 15:09:06 -0600

    Small BridgeCrew compliance changes

--- src/main.tf
+++ src/main.tf
@@ -6,17 +6,20 @@ locals {
 resource "azurerm_resource_group" "main" {
   name     = var.md_metadata.name_prefix
   location = var.storage.region
+  tags     = var.md_metadata.default_tags
 }
 
 resource "azurerm_storage_account" "main" {
-  name                = local.storage_account_name
-  resource_group_name = azurerm_resource_group.main.name
-  location            = azurerm_resource_group.main.location
-
-  account_tier             = "Standard"
-  account_kind             = "StorageV2"
-  access_tier              = "Hot"
-  account_replication_type = var.redundancy.replication_type
+  name                              = local.storage_account_name
+  resource_group_name               = azurerm_resource_group.main.name
+  location                          = azurerm_resource_group.main.location
+  account_tier                      = "Standard"
+  account_kind                      = "StorageV2"
+  access_tier                       = "Hot"
+  account_replication_type          = var.redundancy.replication_type
+  public_network_access_enabled     = false
+  infrastructure_encryption_enabled = true
+  tags                              = var.md_metadata.default_tags
 
   identity {
     type = "SystemAssigned"
