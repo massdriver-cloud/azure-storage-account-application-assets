856367cd1a3acee946c395b4f3fa716c3ff051ec
Will Beebe <will@containerlabs.io>
2023-01-08 19:44:33 -0800

    remove broken alarm (#19)

--- src/monitoring.tf
+++ src/monitoring.tf
@@ -1,13 +1,5 @@
 locals {
   automated_alarms = {
-    availability_metric_alert = {
-      severity    = "1"
-      frequency   = "PT1M"
-      window_size = "PT5M"
-      operator    = "GreaterThan"
-      aggregation = "Average"
-      threshold   = 90
-    }
     success_e2e_latency_metric_alert = {
       severity    = "1"
       frequency   = "PT1M"
@@ -38,32 +30,6 @@ module "alarm_channel" {
   source              = "github.com/massdriver-cloud/terraform-modules//azure-alarm-channel?ref=40d6e54"
   md_metadata         = var.md_metadata
   resource_group_name = azurerm_resource_group.main.name
-}
-
-module "availability_metric_alert" {
-  count                   = local.monitoring_enabled
-  source                  = "github.com/massdriver-cloud/terraform-modules//azure-monitor-metrics-alarm?ref=40d6e54"
-  scopes                  = [azurerm_storage_account.main.id]
-  resource_group_name     = azurerm_resource_group.main.name
-  monitor_action_group_id = module.alarm_channel.id
-  severity                = local.alarms.availability_metric_alert.severity
-  frequency               = local.alarms.availability_metric_alert.frequency
-  window_size             = local.alarms.availability_metric_alert.window_size
-
-  depends_on = [
-    azurerm_storage_account.main
-  ]
-
-  md_metadata  = var.md_metadata
-  display_name = "Availability"
-  message      = "Low availability"
-
-  alarm_name       = "${var.md_metadata.name_prefix}-lowAvailability"
-  operator         = local.alarms.availability_metric_alert.operator
-  metric_name      = "Availability"
-  metric_namespace = "Microsoft.Storage/storageAccounts"
-  aggregation      = local.alarms.availability_metric_alert.aggregation
-  threshold        = local.alarms.availability_metric_alert.threshold
 }
 
 module "success_e2e_latency_metric_alert" {
